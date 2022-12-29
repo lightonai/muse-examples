85bb0e8e073e6b47145804507c651f8f32fabcd8
Iacopo Poli <iapoli@lighton.ai>
2022-12-29 11:11:59 +0100

    :white_check_mark: Remove tests for public key length limits

--- test/test.py
+++ test/test.py
@@ -1,14 +1,7 @@
-from enum import Enum
 import json
 import unittest
 
 from lightonmuse import Tokenize
-
-
-class PublicLimits(Enum):
-    INPUT_TOKENS = 384
-    OUTPUT_TOKENS = 51
-    MAX_BATCH_SIZE = 1
 
 
 class TestCreateEndpoint(unittest.TestCase):
@@ -23,11 +16,6 @@ class TestCreateEndpoint(unittest.TestCase):
             print(example['title'])
             tokenizer = Tokenize(example['model'])
             response, cost, rid = tokenizer(example['request']['text'])
-            assert response[0]['execution_metadata']['cost']['tokens_input'] < PublicLimits.INPUT_TOKENS.value, \
-                f"Example {example['title']} has " \
-                f"input tokens={response[0]['execution_metadata']['cost']['tokens_input']} > {PublicLimits.OUTPUT_TOKENS.value}"
-            assert example['request']['params']['n_tokens'] < PublicLimits.OUTPUT_TOKENS.value, \
-                f"Example {example['title']} has output tokens={example['request']['params']['n_tokens']} > {PublicLimits.OUTPUT_TOKENS.value}"
             assert not isinstance(example['request'], list), f"Example {example['title']} has batch size > 1"
             assert not isinstance(example['request']['text'], list), f"Example {example['title']} has batch size > 1"
             assert "n_completions" not in example['request']['params'] or \
